6e3f92ed104ef809dffe5dde5811c34d717888c9
Andrej Babiak <andrejbabiak@int.pl>
2021-06-01 19:50:44 +0200

    first commit

---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,42 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+// TODO - add other action types
+
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION});
+// TODO - add other action creators
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE:
+      
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+      // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          [action.payload.type]: parseInt(action.payload.value),
+        }, 
+      };
+    default:
+      return statePart;
+  }
+}

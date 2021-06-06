1e8645aa7e51a8b0fcc436fea118f0dc5a3e178b
Andrej Babiak <andrejbabiak@int.pl>
2021-06-06 11:01:27 +0200

    add all default forms

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
 
-
-const OrderOptionNumber = props => (
-  <div>{props.name}</div>
+const OrderOptionNumber = ({currentValue, limits, price, setOptionValue}) => (
+  <div className={styles.number}>
+    <input type='number' className={styles.inputSmall}
+      min={limits.min} max={limits.max}
+      value={currentValue}
+      onChange={event => setOptionValue(event.currentTarget.value)}>    
+    </input>
+    (+{formatPrice(price)}) 
+  </div>
 );
   
 OrderOptionNumber.propTypes = {
-  name: PropTypes.string,
+  currentValue: PropTypes.any,
+  limits: PropTypes.object,
+  setOptionValue: PropTypes.func,
+  price: PropTypes.string,
 };
     
   

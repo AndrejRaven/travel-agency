1d7673f03b9b8366c83a62cdf4f1fac4feb0364f
Andrej Babiak <andrejbabiak@int.pl>
2021-06-07 18:40:51 +0200

    add text and date option for option form

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+
+const OrderOptionText = () => {
+    
+  return (
+    <div className={styles.component}>
+      <input type='text' className={styles.input} />
+    </div>  
+  );
+};
+
+
+export default OrderOptionText;
